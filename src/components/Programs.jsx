8cae12a18d8fe642993b6d813f952dd71bd252d1
Adal1942 <adautosombra1942@gmail.com>
2022-10-01 20:14:24 -0300

    secund commit

--- src/components/Programs.jsx
+++ src/components/Programs.jsx
@@ -1,9 +1,31 @@
 import '../App.css';
 import React from 'react'
+import {programsData} from '../data/programsData';
+import RightArrow from '../assets/rightArrow.png';
 
 function Programs() {
   return (
-    <div>Programs</div>
+    <div className="Programs" id="programs">
+        <div className="programs-header">
+            <span className="stroke-text">Explore our</span>
+            <span>Programs</span>
+            <span className="stroke-text">to shape you</span>
+        </div>
+        
+        <div className="program-categories">
+            {programsData.map((program)=>(
+                <div className="category">
+                    {program.image}
+                    <span>{program.heading}</span>
+                    <span>{program.details}</span>
+                    <div className="join-now">
+                        <span>Join Now</span>
+                        <img src={RightArrow}/>
+                    </div>
+                </div>
+            ))}
+        </div>
+    </div>
   )
 }
 
